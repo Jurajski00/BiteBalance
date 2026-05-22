8b496524fb681f8c4ce1f80f8e848a870d295720
Jurajski00 <232651640+Jurajski00@users.noreply.github.com>
2026-05-22 16:12:29 +0200

    feat: add register page

---
+++ register.php
@@ -0,0 +1,78 @@
+<?php
+    require "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Diet tracker</title>
+</head>
+<body class="d-flex justify-content-center align-items-center min-vh-100">
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-12">
+                <h1 class="fw-bold text-success text-center">Register</h1>
+            </div>
+        </div>
+
+        <div class="row justify-content-center>
+            <div class="col-12">
+
+                <form class="p-4 bg-white rounded-5 shadow-lg">
+                    <div class="row d-flex mb-3">
+                        <div class="col-6">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" id="name" class="form-control" required>
+                         </div>
+
+                        <div class="col-6">
+                            <label for="surname" class="form-label">Surname</label>
+                            <input type="text" id="surname" class="form-control" required>
+                        </div>
+                    </div>
+
+                    <div class="row d-flex mb-3">
+                        <div class="col-4">
+                            <label for="weight" class="form-label">Weight</label>
+                            <input type="number" id="weight" class="form-control" required>
+                        </div>
+                        
+                        <div class="col-4">
+                            <label for="height" class="form-label">Height</label>
+                            <input type="number" id="height" class="form-control" required>
+                        </div>
+
+                        <div class="col-4">
+                            <label for="age" class="form-label">Age</label>
+                            <input type="number" id="age" class="form-control" required>
+                        </div>
+                    </div>
+
+                    <div class="col-12 mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" id="username" class="form-control" required>
+                    </div>
+
+                    <div class="col-12 mb-3">
+                        <label for="email" class="form-label">E-mail</label>
+                        <input type="email" id="email" class="form-control" required>
+                    </div>
+
+                    <div class="col-12 mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" id="password" class="form-control" required>
+                    </div>
+
+                    <div class="col-12 d-flex justify-content-end">
+                        <button type="submit" class="btn btn-success px-4 ">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
